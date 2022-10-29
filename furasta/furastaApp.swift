98f1fb760c5698473b134605bdd62c1db9815e30
justin reid <hi@justinre.id>
2022-10-29 13:25:30 +0100

    Initial Commit

---
+++ furasta/furastaApp.swift
@@ -0,0 +1,17 @@
+//
+//  furastaApp.swift
+//  furasta
+//
+//  Created by justin on 29/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct furastaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
